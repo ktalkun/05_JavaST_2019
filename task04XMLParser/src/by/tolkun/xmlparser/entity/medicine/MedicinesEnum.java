c76c98b8df41bb35edb92c74c1a930d7558d4c67
Kirill Tolkun <tolkun_14@inbox.ru>
2019-04-14 13:17:35 +0300

    Add entity classes

---
+++ task04XMLParser/src/by/tolkun/xmlparser/entity/medicine/MedicinesEnum.java
@@ -0,0 +1,85 @@
+package by.tolkun.xmlparser.entity.medicine;
+
+/**
+ * Class to describe tag names.
+ *
+ * @author Kirill Tolkun
+ */
+public enum MedicinesEnum {
+    /**
+     * Name of tag <medicines>.
+     */
+    MEDICINES,
+    /**
+     * Name of tag <medicine>.
+     */
+    MEDICINE,
+    /**
+     * Name of tag <name>.
+     */
+    NAME,
+    /**
+     * Name of tag <group>.
+     */
+    GROUP,
+    /**
+     * Name of tag <analogs>.
+     */
+    ANALOGS,
+    /**
+     * Name of tag <versions>.
+     */
+    VERSIONS,
+    /**
+     * Name of tag <version>.
+     */
+    VERSION,
+    /**
+     * Name of tag <company>.
+     */
+    COMPANY,
+    /**
+     * Name of tag <company-certificate>.
+     */
+    COMPANY_CERTIFICATE,
+    /**
+     * Name of tag <package>.
+     */
+    PACKAGE,
+    /**
+     * Name of tag <quantity>.
+     */
+    QUANTITY,
+    /**
+     * Name of tag <price>.
+     */
+    PRICE,
+    /**
+     * Name of tag <dosage>.
+     */
+    DOSAGE,
+    /**
+     * Name of tag <size>.
+     */
+    SIZE,
+    /**
+     * Name of tag <period>.
+     */
+    PERIOD,
+    /**
+     * Name of tag <certificate>.
+     */
+    CERTIFICATE,
+    /**
+     * Name of tag <date-issue>.
+     */
+    DATE_ISSUE,
+    /**
+     * Name of tag <date-expiration>.
+     */
+    DATE_EXPIRATION,
+    /**
+     * Name of tag <reg-organization>.
+     */
+    REG_ORGANIZATION
+}
